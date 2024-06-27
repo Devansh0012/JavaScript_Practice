8261a3991a3c851643d1fcd2906d9b804e039000
Devansh0012 <devanshdubey0012@gmail.com>
2024-06-28 00:36:43 +0530

    info on conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -4,3 +4,20 @@ console.log(typeof score) // number
 let scoreString = score.toString() // convert number to string
 console.log(typeof scoreString) // string
 
+let stringScore = '33'
+console.log(typeof stringScore) // string
+
+let stringScoreNumber = parseInt(stringScore) // convert string to number
+console.log(typeof stringScoreNumber) // number
+
+/*
+Conversion Operations
+- Number to String: number.toString()
+- String to Number: parseInt(string), parseFloat(string)
+- String to Number: Number(string)
+- Number to Boolean: Boolean(number)
+- String to Boolean: Boolean(string)
+- Number to String: String(number)
+- Boolean to String: String(boolean)
+- Boolean to Number: Number(boolean)
+*/
